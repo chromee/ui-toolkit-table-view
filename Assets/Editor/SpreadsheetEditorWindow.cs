de9c2d360ca7c1ea2f0c7f148576bbb26646f7a2
chorome <odk.mmmm@gmail.com>
2024-06-30 21:16:41 +0900

    リファクタと見た目の調整、行追加機能の第一歩

--- Assets/Editor/SpreadsheetEditorWindow.cs
+++ Assets/Editor/SpreadsheetEditorWindow.cs
@@ -10,13 +10,10 @@ namespace Editor
 {
     public class SpreadsheetEditorWindow : EditorWindow
     {
-        private int _columnCount = 5;
-        private int _rowCount = 5;
-        private float[] _columnWidths;
         private VisualElement[] _headerCells;
-        private Cell[][] _cells;
-        private VisualElement _table;
-        private VisualElement _headerRow;
+        private readonly List<Cell[]> _dataRows = new();
+        private Cell[] _emptyRow;
+        private float[] _columnWidths;
 
         private bool _isSelecting;
         private Cell _startSelectedCell;
@@ -44,38 +41,16 @@ namespace Editor
             var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/SpreadsheetEditor.uss");
             rootVisualElement.styleSheets.Add(styleSheet);
 
-            _columnWidths = new float[_columnCount];
-            _headerCells = new VisualElement[_columnCount];
-            _cells = new Cell[_rowCount][];
-
-            // Initialize column widths
-            for (var i = 0; i < _columnCount; i++) _columnWidths[i] = 100f;
-
-            // Create a table
-            _table = new VisualElement();
-            _table.style.flexDirection = FlexDirection.Column;
-            _table.style.flexGrow = 1;
-
-            // Create header row with resizable columns
-            _headerRow = new VisualElement();
-            _headerRow.style.flexDirection = FlexDirection.Row;
-            for (var j = 0; j < _columnCount; j++)
+            var tableInfo = new TableInfo("Table", new[]
             {
-                var headerCell = CreateHeaderCell(j);
-                _headerCells[j] = headerCell;
-                _headerRow.Add(headerCell);
-            }
+                new ColInfo(typeof(string), "String", 100f),
+                new ColInfo(typeof(int), "Int", 100f),
+                new ColInfo(typeof(float), "Float", 100f),
+                new ColInfo(typeof(bool), "Bool", 100f),
+                new ColInfo(typeof(string), "String", 100f),
+            });
 
-            _table.Add(_headerRow);
-
-            // Create data rows
-            for (var i = 0; i < _rowCount; i++)
-            {
-                var row = CreateDataRow(i);
-                _table.Add(row);
-            }
-
-            rootVisualElement.Add(_table);
+            rootVisualElement.Add(CreateTable(tableInfo));
 
             _selectMarker = new Marker(rootVisualElement, "select-marker");
             _selectRangeMarker = new Marker(rootVisualElement, "select-range-marker");
@@ -96,95 +71,141 @@ namespace Editor
 
         #endregion
 
-        #region shortcuts
-
-        private void SetupRootVisualElementForKeyboardInput()
-        {
-            rootVisualElement.focusable = true;
-            rootVisualElement.pickingMode = PickingMode.Position;
-            rootVisualElement.Focus();
-        }
-
-        private void RegisterShortcuts()
-        {
-            rootVisualElement.RegisterCallback<KeyDownEvent>(OnKeyDown, TrickleDown.TrickleDown);
-            rootVisualElement.RegisterCallback<KeyUpEvent>(OnKeyUp, TrickleDown.TrickleDown);
-        }
-
-        private void UnregisterShortcuts()
-        {
-            rootVisualElement.UnregisterCallback<KeyDownEvent>(OnKeyDown, TrickleDown.TrickleDown);
-            rootVisualElement.UnregisterCallback<KeyUpEvent>(OnKeyUp, TrickleDown.TrickleDown);
-        }
-
-        private void OnKeyDown(KeyDownEvent ev)
-        {
-            _pressedKeys.Add(ev.keyCode);
-            if (_pressedKeys.Contains(KeyCode.LeftControl) && _pressedKeys.Contains(KeyCode.C)) CopyCell();
-            else if (_pressedKeys.Contains(KeyCode.LeftControl) && _pressedKeys.Contains(KeyCode.V)) PasteCell();
-            else if (_pressedKeys.Contains(KeyCode.LeftControl) && _pressedKeys.Contains(KeyCode.LeftShift) && _pressedKeys.Contains(KeyCode.Z)) Redo();
-            else if (_pressedKeys.Contains(KeyCode.LeftControl) && _pressedKeys.Contains(KeyCode.Z)) Undo();
-            else if (_pressedKeys.Contains(KeyCode.LeftControl) && _pressedKeys.Contains(KeyCode.Y)) Redo();
-            else if (_pressedKeys.Contains(KeyCode.Escape)) CancelAll();
-        }
-
-        private void OnKeyUp(KeyUpEvent ev)
-        {
-            if (_pressedKeys.Contains(ev.keyCode)) _pressedKeys.Remove(ev.keyCode);
-        }
-
-        private void CancelAll()
-        {
-            CancelCopy();
-            CancelSelecting();
-        }
-
-        #endregion
-
         #region create
 
-        private VisualElement CreateHeaderCell(int columnIndex)
+        private VisualElement CreateTable(TableInfo tableInfo, int rowCount = 10)
+        {
+            var table = new VisualElement();
+            table.AddToClassList("table");
+
+            var colLength = tableInfo.ColInfos.Length;
+
+            _headerCells = new VisualElement[colLength];
+            _columnWidths = new float[colLength];
+            for (var i = 0; i < colLength; i++) _columnWidths[i] = 100f;
+
+            table.Add(CreateHeaderRow(tableInfo.ColInfos));
+
+            // Create data rows
+            for (var i = 0; i < rowCount; i++)
+            {
+                var row = CreateDataRow(tableInfo.ColInfos);
+                table.Add(row);
+            }
+
+            // Create empty row
+            var emptyRow = CreateEmptyRow(tableInfo.ColInfos);
+            table.Add(emptyRow);
+
+            return table;
+        }
+
+        private VisualElement CreateHeaderRow(ColInfo[] colInfos)
+        {
+            var headerRow = new VisualElement();
+            headerRow.AddToClassList("row");
+
+            var topIndexCell = new VisualElement();
+            topIndexCell.AddToClassList("cell");
+            topIndexCell.AddToClassList("index-cell");
+            topIndexCell.AddToClassList("top-index-cell");
+            headerRow.Add(topIndexCell);
+
+            for (var i = 0; i < colInfos.Length; i++)
+            {
+                var headerCell = CreateHeaderCell(colInfos[i], i);
+                _headerCells[i] = headerCell;
+                headerRow.Add(headerCell);
+            }
+
+            return headerRow;
+        }
+
+        private VisualElement CreateDataRow(ColInfo[] colInfos)
+        {
+            _dataRows.Add(new Cell[colInfos.Length]);
+            var rowIndex = _dataRows.Count - 1;
+
+            var row = new VisualElement();
+            row.AddToClassList("row");
+
+            var indexCell = new VisualElement();
+            indexCell.AddToClassList("cell");
+            indexCell.AddToClassList("index-cell");
+            indexCell.Add(new Label(_dataRows.Count.ToString()));
+            row.Add(indexCell);
+
+            for (var i = 0; i < colInfos.Length; i++)
+            {
+                var colInfo = colInfos[i];
+
+                var cell = Type.GetTypeCode(colInfo.Type) switch
+                {
+                    TypeCode.String => CreateCell(rowIndex, i, $"Cell {rowIndex},{i}"),
+                    TypeCode.Int32 => CreateCell(rowIndex, i, rowIndex),
+                    TypeCode.Single => CreateCell(rowIndex, i, rowIndex * 1.1f),
+                    TypeCode.Boolean => CreateCell(rowIndex, i, rowIndex % 2 == 0),
+                    _ => CreateCell(rowIndex, i, $"Cell {rowIndex},{i}"),
+                };
+
+                _dataRows[rowIndex][i] = cell;
+
+                row.Add(cell.Element);
+                cell.OnValueChangedFromEdit += (from, to) => AddUndoCommand(cell, from, to);
+            }
+
+            return row;
+        }
+
+        private VisualElement CreateEmptyRow(ColInfo[] colInfos)
+        {
+            _emptyRow = new Cell[colInfos.Length];
+
+            var row = new VisualElement();
+            row.AddToClassList("row");
+
+            var addRowButton = new Button { text = "+" };
+            addRowButton.AddToClassList("add-row-button");
+            row.Add(addRowButton);
+
+            var rowIndex = _dataRows.Count - 1;
+            for (var i = 0; i < colInfos.Length; i++)
+            {
+                var colInfo = colInfos[i];
+
+                var cell = Type.GetTypeCode(colInfo.Type) switch
+                {
+                    TypeCode.String => CreateCell(rowIndex, i, string.Empty),
+                    TypeCode.Int32 => CreateCell(rowIndex, i, 0),
+                    TypeCode.Single => CreateCell(rowIndex, i, 0f),
+                    TypeCode.Boolean => CreateCell(rowIndex, i, false),
+                    _ => CreateCell(0, i, string.Empty),
+                };
+
+                _emptyRow[i] = cell;
+
+                row.Add(cell.Element);
+                cell.OnValueChangedFromEdit += (from, to) => AddUndoCommand(cell, from, to);
+            }
+
+            return row;
+        }
+
+        private VisualElement CreateHeaderCell(ColInfo colInfo, int columnIndex)
         {
             var headerCell = new VisualElement();
             headerCell.AddToClassList("header-cell");
             headerCell.style.width = _columnWidths[columnIndex];
 
-            var headerLabel = new Label($"Header {columnIndex}");
-            headerLabel.AddToClassList("header-label");
+            var headerLabel = new Label(colInfo.Name);
             headerCell.Add(headerLabel);
 
             var resizer = new VisualElement();
-            resizer.AddToClassList("resizer");
+            resizer.AddToClassList("header-cell-resize-handle");
             resizer.RegisterCallback<MouseDownEvent>(evt => StartResizing(evt, columnIndex));
-
             headerCell.Add(resizer);
 
             return headerCell;
-        }
-
-        private VisualElement CreateDataRow(int rowIndex)
-        {
-            var row = new VisualElement();
-            row.style.flexDirection = FlexDirection.Row;
-            _cells[rowIndex] = new Cell[_columnCount];
-
-            for (var colIndex = 0; colIndex < _columnCount; colIndex++)
-            {
-                Cell cell;
-
-                if (colIndex == 0) cell = CreateCell(rowIndex, colIndex, $"Cell {rowIndex},{colIndex}");
-                else if (colIndex == 1) cell = CreateCell(rowIndex, colIndex, rowIndex);
-                else if (colIndex == 2) cell = CreateCell(rowIndex, colIndex, rowIndex * 1.1f);
-                else cell = CreateCell(rowIndex, colIndex, rowIndex % 2 == 0);
-
-                if (cell != null)
-                {
-                    row.Add(cell.Element);
-                    cell.OnValueChangedFromEdit += (from, to) => AddUndoCommand(cell, from, to);
-                }
-            }
-
-            return row;
         }
 
         private Cell CreateCell<T>(int rowIndex, int colIndex, T value)
@@ -198,8 +219,6 @@ namespace Editor
             });
 
             cell.Element.RegisterCallback<MouseEnterEvent>(_ => { Selecting(cell); });
-
-            _cells[rowIndex][colIndex] = cell;
 
             return cell;
         }
@@ -260,7 +279,7 @@ namespace Editor
             for (var i = top; i <= bottom; i++)
             {
                 selectedCells[i - top] = new Cell[right - left + 1];
-                for (var j = left; j <= right; j++) selectedCells[i - top][j - left] = _cells[i][j];
+                for (var j = left; j <= right; j++) selectedCells[i - top][j - left] = _dataRows[i][j];
             }
 
             return selectedCells;
@@ -433,10 +452,11 @@ namespace Editor
             var delta = evt.mousePosition.x - _initialMousePosition.x;
             _columnWidths[_resizingColumnIndex] = Mathf.Max(50, _initialColumnWidth + delta);
             _headerCells[_resizingColumnIndex].style.width = _columnWidths[_resizingColumnIndex];
+            _emptyRow[_resizingColumnIndex].Width = _columnWidths[_resizingColumnIndex];
 
-            for (var i = 0; i < _rowCount; i++)
+            foreach (var row in _dataRows)
             {
-                var cell = _cells[i][_resizingColumnIndex];
+                var cell = row[_resizingColumnIndex];
                 cell.Width = _columnWidths[_resizingColumnIndex];
                 if (_selectMarker.IsVisible) _selectMarker.Fit(_startSelectedCell);
                 if (_selectRangeMarker.IsVisible) _selectRangeMarker.Fit(_startSelectedCell, _endSelectedCell);
@@ -454,7 +474,82 @@ namespace Editor
         }
 
         #endregion
+
+        #region shortcuts
+
+        private void SetupRootVisualElementForKeyboardInput()
+        {
+            rootVisualElement.focusable = true;
+            rootVisualElement.pickingMode = PickingMode.Position;
+            rootVisualElement.Focus();
+        }
+
+        private void RegisterShortcuts()
+        {
+            rootVisualElement.RegisterCallback<KeyDownEvent>(OnKeyDown, TrickleDown.TrickleDown);
+            rootVisualElement.RegisterCallback<KeyUpEvent>(OnKeyUp, TrickleDown.TrickleDown);
+        }
+
+        private void UnregisterShortcuts()
+        {
+            rootVisualElement.UnregisterCallback<KeyDownEvent>(OnKeyDown, TrickleDown.TrickleDown);
+            rootVisualElement.UnregisterCallback<KeyUpEvent>(OnKeyUp, TrickleDown.TrickleDown);
+        }
+
+        private void OnKeyDown(KeyDownEvent ev)
+        {
+            _pressedKeys.Add(ev.keyCode);
+            if (_pressedKeys.Contains(KeyCode.LeftControl) && _pressedKeys.Contains(KeyCode.C)) CopyCell();
+            else if (_pressedKeys.Contains(KeyCode.LeftControl) && _pressedKeys.Contains(KeyCode.V)) PasteCell();
+            else if (_pressedKeys.Contains(KeyCode.LeftControl) && _pressedKeys.Contains(KeyCode.LeftShift) && _pressedKeys.Contains(KeyCode.Z)) Redo();
+            else if (_pressedKeys.Contains(KeyCode.LeftControl) && _pressedKeys.Contains(KeyCode.Z)) Undo();
+            else if (_pressedKeys.Contains(KeyCode.LeftControl) && _pressedKeys.Contains(KeyCode.Y)) Redo();
+            else if (_pressedKeys.Contains(KeyCode.Escape)) CancelAll();
+        }
+
+        private void OnKeyUp(KeyUpEvent ev)
+        {
+            if (_pressedKeys.Contains(ev.keyCode)) _pressedKeys.Remove(ev.keyCode);
+        }
+
+        private void CancelAll()
+        {
+            CancelCopy();
+            CancelSelecting();
+        }
+
+        #endregion
     }
+
+    #region info
+
+    public class TableInfo
+    {
+        public readonly string Name;
+        public readonly ColInfo[] ColInfos;
+
+        public TableInfo(string name, ColInfo[] colInfos)
+        {
+            Name = name;
+            ColInfos = colInfos;
+        }
+    }
+
+    public class ColInfo
+    {
+        public readonly Type Type;
+        public readonly string Name;
+        public readonly float Width;
+
+        public ColInfo(Type type, string name, float width)
+        {
+            Type = type;
+            Name = name;
+            Width = width;
+        }
+    }
+
+    #endregion
 
     #region elements
 
@@ -719,22 +814,6 @@ namespace Editor
             Element.style.top = startPos.y - rootBound.y;
             Element.style.width = endPos.x - startPos.x - 1;
             Element.style.height = endPos.y - startPos.y - 1;
-        }
-    }
-
-    public class Col
-    {
-        public readonly VisualElement Element;
-        public readonly List<Cell> Cells = new();
-
-        public float Width
-        {
-            get => Element.resolvedStyle.width;
-            set
-            {
-                Element.style.width = value;
-                foreach (var cell in Cells) cell.Width = value;
-            }
         }
     }
 
